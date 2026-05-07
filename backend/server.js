4d51a4ea27b861d51af9869ae56a7e1f61b87700
sreemaye-2006 <Sirisreemaye@gmail.com>
2026-05-07 10:31:56 +0530

    fixed cors

--- backend/server.js
+++ backend/server.js
@@ -14,8 +14,8 @@ config();
 const app=exp()
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL || "http://localhost:5173"],
-    credentials:true  //enables to send the token back to clients
+    origin: "https://blog-two-silk-88.vercel.app",
+    credentials: true,
   })
 );
 app.use(cookieParser()) 
